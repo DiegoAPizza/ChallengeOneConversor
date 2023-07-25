d67b33fb09f751c5a5216fca8ff4d965388bf29f
DiegoApizza <diegoantonio40@live.com>
2023-07-25 12:28:10 -0300

    Mejora considerable del desafio
    mejora en la parte visual

--- conversorUniversal/src/conversorUniversal/conversorUniversal.java
+++ conversorUniversal/src/conversorUniversal/conversorUniversal.java
@@ -1,6 +1,7 @@
 package conversorUniversal;
 import javax.swing.*;
 import java.text.DecimalFormat;
+import java.util.Iterator;
 	
 
 public class conversorUniversal  {
@@ -8,25 +9,33 @@ public class conversorUniversal  {
 	public static void main(String[] args) throws Exception{
 		
 		Object[] opciones={"Convertidor de Divisas","Convertidor de Temperatura","Convertidor de Tiempo(min,hs,disas)","Convertidor de Bytes","Convertidor de Longitudes","Salir"};
-		
-		 int eleccion;
+		String opcionSeleccionada = (String) JOptionPane.showInputDialog(
+	            null,
+	            "¿Qué operación desea realizar?",
+	            "Conversor Universal",
+	            JOptionPane.PLAIN_MESSAGE,
+	            null,
+	            opciones,
+	            opciones[0]
+	        );
+			int posicion=0;
+				for (int i = 0; i < opciones.length; i++) {
+						if (opciones[i].equals(opcionSeleccionada)) {
+							posicion = i;
+							break;
+						}
+					}
 	        do {
-	            eleccion = JOptionPane.showOptionDialog(
-	                null,
-	                "¿Qué operación desea realizar?",
-	                "Conversor Universal",
-	                JOptionPane.DEFAULT_OPTION,
-	                JOptionPane.PLAIN_MESSAGE,
-	                null,
-	                opciones,
-	                opciones[0]
-	            );
-
+	        	 
+	        	
 	            // Procesar la elección del usuario
-	            switch (eleccion) {
+	            switch (posicion) {
 	                case 0:
 	                    // Lógica para Convertidor de Divisas
 	                    JOptionPane.showMessageDialog(null, "Has seleccionado Convertidor de Divisas.");
+	                    boolean continuar=true;
+	                    DecimalFormat formatDiv=new DecimalFormat("#.##");
+	                    convertidor_de_monedas(continuar,formatDiv);
 	                    break;
 	                case 1:
 	                    // Lógica para Convertidor de Temperatura
@@ -53,8 +62,49 @@ public class conversorUniversal  {
 	                    JOptionPane.showMessageDialog(null, "Opción inválida. Por favor, elige una opción válida.");
 	                    break;
 	            }
-	        } while (eleccion != 5); // Continuar el menú hasta que el usuario seleccione "Salir" (opción 5)
+	        } while (posicion != 5); // Continuar el menú hasta que el usuario seleccione "Salir" (opción 5)
 	    }
+
+	public static void convertidor_de_monedas(boolean continuar, DecimalFormat formatDiv) {
+		while(continuar) {
+			String[] divisasDisponibles= {"ARG a USD","ARG a EUR","ARG a JPY","ARG a GBP","ARG a KRW","ARG a BTC"};
+			String divisa_a_Cambiar=(String) JOptionPane.showInputDialog(null,"Seleccione  una Opcion","Conversor de Divisas",JOptionPane.QUESTION_MESSAGE,null,divisasDisponibles,divisasDisponibles[0]);
+			String cantidad_De_Monedas=JOptionPane.showInputDialog(null,"Ingrese Cantidad que desee cambiar de Pesos Arg");
+			double moneda=0.00;
+			try {
+				moneda=Double.parseDouble(cantidad_De_Monedas);
+			} catch (NumberFormatException error) {
+				JOptionPane.showMessageDialog(null,"Valor No valido para esta Operacion");
+			}
+			if(divisa_a_Cambiar.equals("ARG a USD")) {
+				double dolar=moneda/550;
+				JOptionPane.showMessageDialog(null, moneda+"ARG a USD son: "+formatDiv.format(dolar)+" Dolares");
+			}
+			
+			else if(divisa_a_Cambiar.equals("ARG a EUR")) {
+				double euro=moneda/299.74;
+				JOptionPane.showMessageDialog(null, moneda+"ARG a EUR son: "+formatDiv.format(euro)+" Euros");
+			}
+			
+			else if(divisa_a_Cambiar.equals("ARG a JPY")) {
+				double yen=moneda/1.92;
+				JOptionPane.showMessageDialog(null, moneda+"ARG a JPY son: "+formatDiv.format(yen)+" Yen");
+			}
+			
+			else if(divisa_a_Cambiar.equals("ARG a GBP")) {
+				double libra=moneda/349.69;
+				JOptionPane.showMessageDialog(null, moneda+"ARG a GBP son: "+formatDiv.format(libra)+" Libra Estarlina");
+			}
+			
+			else if(divisa_a_Cambiar.equals("ARG a KRW")) {
+				double won=moneda/0.21;
+				JOptionPane.showMessageDialog(null, moneda+"ARG a KRW son: "+formatDiv.format(won)+" Won");
+			}
+			
+			
+		}
+		
+	}
 	
 	
 	}
